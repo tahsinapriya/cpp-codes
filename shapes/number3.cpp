61769adcf2b786b3a7e8949016bf71b9c9f90c11
tahsinapriya <tahsinapriya@gmail.com>
2021-10-23 21:26:07 +0600

    Add more shapes (number3)

---
+++ shapes/number3.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int row;
+    cin>>row;
+    for(int i=1; i<=row; i++){
+        for(int j=1; j<=row ; j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}    //
+// Created by rawn on 10/23/21.
+//
+/*
+input
+2
+output
+1 1
+2 2
+*/
+
