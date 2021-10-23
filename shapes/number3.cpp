35f377636dbd0b1996e8c0da1eb6eae4405dd9b7
tahsinapriya <tahsinapriya@gmail.com>
2021-10-23 21:30:34 +0600

    Add more shapes (number3)

--- shapes/number3.cpp
+++ shapes/number3.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main() {
     int row;
-    cin>>row;
-    for(int i=1; i<=row; i++){
-        for(int j=1; j<=row ; j++){
-            cout<<i;
+    cin >> row;
+    for(int i = 1; i <= row; i++){
+        for(int j = 1; j <= row ; j++){
+            cout << i;
         }
-        cout<<endl;
+        cout << endl;
     }
 }    //
 // Created by rawn on 10/23/21.
